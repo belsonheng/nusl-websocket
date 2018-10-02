d0e7a1988922e360b73df37b9570f4bc857ae78e
Heng Belson <ccehb@nus.edu.sg>
2018-10-02 16:15:01 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const amqp = require('amqp');
+const jwtDecode = require('jwt-decode');
+
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/:hostname', function(request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+});
+
+rabbitMQ = amqp.createConnection({ host: 'localhost' });
+
+rabbitMQ.on('ready', function() {
+  io.on('connection', function(socket) {
+    var ip = socket.request.connection.remoteAddress.replace(/^.*:/, '');
+    var hostname = socket.request.headers.referer.match(/[^/]*(?=(\/)?$)/)[0];
+    console.log(ip + ' connected with hostname ' + hostname);
+    socket.emit('message', hostname + ' connected to server');
+    var queue = rabbitMQ.queue(hostname);
+    queue.bind(hostname);
+    queue.subscribe(function(message) {
+      if (message['logout']) {
+        console.log('user has signed out')
+        socket.emit('expiry', Date.now())
+      } else {
+        var timestamp = jwtDecode(message['jwt']).exp*1000
+        console.log(`expiry in ${timestamp} ms`);
+        socket.emit('expiry', timestamp);
+      }
+    });
+  });
+});
+
+server.listen(3000, function() {
+  console.log('[x] nusl-websocket server started on port 3000');
+});
