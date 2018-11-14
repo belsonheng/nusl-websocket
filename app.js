40a5c291b0938c45285b44ed2a70ca5102ac85d1
Belson Heng <belsonheng@gmail.com>
2018-11-14 09:09:07 +0800

    fix rabbitMQ.on('ready') bug

--- app.js
+++ app.js
@@ -16,27 +16,36 @@ app.get('/:hostname', function(request, response) {
   response.sendFile(__dirname + '/public/index.html');
 });
 
-rabbitMQ = amqp.createConnection({ host: 'localhost' });
-
-rabbitMQ.on('ready', function() {
-  io.on('connection', function(socket) {
-    var ip = (socket.request.headers['x-forwarded-for'] || socket.request.connection.remoteAddress).replace(/^.*:/, '');
-    var hostname = socket.request.headers.referer.match(/[^/]*(?=(\/)?$)/)[0];
-    console.log(ip + ' connected with hostname ' + hostname);
-    socket.emit('message', hostname + ' connected to server');
-    var queue = rabbitMQ.queue(hostname);
+io.on('connection', function(socket) {
+  var rabbitMQ = amqp.createConnection({ host: 'localhost' });
+  var queue = null;
+  var ip = (socket.request.headers['x-forwarded-for'] || socket.request.connection.remoteAddress).replace(/^.*:/, '');
+  var hostname = socket.request.headers.referer.match(/[^/]*(?=(\/)?$)/)[0];
+  console.log(ip + ' connected with hostname ' + hostname);
+  
+  rabbitMQ.once('ready', function() {
+    socket.emit('message', hostname + ' connected to server ' + socket.id);
+    queue = rabbitMQ.queue(hostname);
     queue.bind(hostname);
     queue.subscribe(function(message) {
       if (message['logout']) {
-        console.log('user has signed out')
-        socket.emit('expiry', Date.now())
-      } else {
-        var timestamp = jwtDecode(message['jwt']).exp*1000
+        console.log('user has signed out');
+        socket.emit('expiry', Date.now());
+      } 
+      if (message['jwt']) {
+        var timestamp = jwtDecode(message['jwt']).exp*1000;
+        console.log(`jwt: ${message['jwt']}`);
         console.log(`expiry in ${timestamp} ms`);
         socket.emit('expiry', timestamp);
       }
     });
-  });
+    
+    socket.on('disconnect', function() {
+      queue.unsubscribe(hostname);
+      rabbitMQ.disconnect();
+      console.log(`${hostname} disconnected`);  
+    });
+  });  
 });
 
 server.listen(process.env.PORT || 8888, function() {
